988924f78cd6284d40c4c2242976e94e689e944e
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-29 18:35:03 +0600

    get all todos controller

---
+++ src/main/java/com/mahmud/spring_reactive_webclient/controller/TodoController.java
@@ -0,0 +1,25 @@
+package com.mahmud.spring_reactive_webclient.controller;
+
+import com.mahmud.spring_reactive_webclient.model.Todo;
+import com.mahmud.spring_reactive_webclient.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    public TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public Flux<Todo> getAllTasks() {
+        return todoService.findAllTasks();
+    }
+}
