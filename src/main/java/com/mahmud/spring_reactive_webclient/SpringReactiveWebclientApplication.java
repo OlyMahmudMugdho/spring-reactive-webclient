847b00b145f9b5edaa349d492138c2fdd8902497
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-29 18:34:03 +0600

    init

---
+++ src/main/java/com/mahmud/spring_reactive_webclient/SpringReactiveWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.spring_reactive_webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveWebclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahmud/spring_reactive_webclient/SpringReactiveWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.spring_reactive_webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveWebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
